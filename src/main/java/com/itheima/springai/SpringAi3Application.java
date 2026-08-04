202f77649ef6708137ae506f2a05a97f9cbb3aca
梁浩然 <3172450367@qq.com>
2026-08-04 12:08:34 +0800

    Initial commit

---
+++ src/main/java/com/itheima/springai/SpringAi3Application.java
@@ -0,0 +1,13 @@
+package com.itheima.springai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAi3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAi3Application.class, args);
+    }
+
+}
